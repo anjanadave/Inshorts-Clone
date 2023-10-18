4a1fc9b8180534aa6cc1b1620fec268ee5248afe
unknown <anju7295>
2023-10-19 05:23:11 +0530

    first commit

---
+++ server/database/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+
+
+const Connection = async(username, password) => {
+    const URL = `mongodb+srv://${username}:${password}@cluster0.r2zh0le.mongodb.net/Inshorts?retryWrites=true&w=majority`;
+    try{
+       await mongoose.connect(URL, { useNewUrlParser: true});
+       console.log('Database connected successfully');
+    }catch (error) {
+        console.log('Error while connecting with the database', error);
+    }
+}
+
+export default Connection;
